2b599ce4eca110bb776d09254dde07c71c27e469
Simen Tennøe <simetenn@gmail.com>
2015-06-30 14:28:08 +0200

    fixed critical bugs

--- uncertainty.py
+++ uncertainty.py
@@ -51,12 +51,15 @@ parameters = {
 
 figureformat = ".png"
 figurepath = "/figures/"
+#fitted_parameters =   ["Rm", "Epas", "gkdr", "kdrsh", "gahp", "gcat",
+#                       "gcal", "ghbar", "catau", "gcanbar"]
+#fitted_parameters = "gkdr"
+#fitted_parameters = ["Rm","gahp"]
+#fitted_parameters =   ["kdrsh", "Epas"]
+fitted_parameters =   ["Rm", "Epas", "gkdr", "kdrsh", "gahp", "gcat",
+                       "gcal", "ghbar", "catau", "gcanbar"]
 
-#fitted_parameters = ['Rm', 'Epas', 'ghbar', 'gcat']
-fitted_parameters = ['Rm', 'Epas']
-
-
-#Quick and dirty parameter space thingys
+#Quick and dirty parameter space things
 def newParameterSpaceNormal(fitted_parameters, parameters=parameters):
     parameter_space = {}
     for param in fitted_parameters:
@@ -72,18 +75,39 @@ def newParameterSpaceUniform(fitted_parameters, parameters=parameters):
     return parameter_space
 
 
+
+def newParameterSpace(fitted_parameters, distribution = lambda parameter: cp.Normal(parameter, abs(std_percentage*parameter)), parameters=parameters):
+    """
+    Generalized parameter space creation
+    """
     
+    parameter_space = {}
+    
+    if type(fitted_parameters) == str:
+        parameter_space[fitted_parameters] = distribution(parameters[fitted_parameters])
+        return parameter_space
+    else:
+        for param in fitted_parameters:
+            parameter_space[param] = distribution(parameters[param])
+    
+        return parameter_space
+
+"""
 def setParameters(parameters):
     for param in parameters:
         setattr(h, param, parameters[param])
         
     h.finitialize()
+"""
 
-def setParameters(parameters, parameter_values):
+def setParameters(parameter_values, parameters = fitted_parameters):
+    if parameter_values.size == 1:
+        parameter_values = [parameter_values]
+    
     if len(parameters) != len(parameter_values):
-        print "Error: Length of parameters and parameter_values is not equal"
+        print "Error: Length of parameters and parameter_values are not equal"
         sys.exit(1)
-        
+
     for i in range(len(parameters)):
         setattr(h, parameters[i], parameter_values[i])
         
@@ -97,11 +121,12 @@ def run(parameters):
 
     _t = toArray(t)
     _V = toArray(V)
-    return _t,_V 
+    return _t,_V
 
-#h("cvode_active(0)")
-def createPCExpansion(parameter_space = parameters, cvode_active=True):
+    
 
+def createPCExpansion(parameter_space, cvode_active=True):
+        
     if not cvode_active:
         h("cvode_active(0)")
     
@@ -121,8 +146,8 @@ def createPCExpansion(parameter_space = parameters, cvode_active=True):
     for s in nodes.T:
         sys.stdout.write("\rRunning Neuron: %2.1f%%" % (i/len(nodes.T)*100))
         sys.stdout.flush()
- 
-        setParameters(fitted_parameters, s)
+        
+        setParameters(s, parameter_space.keys())
         h.run()
         
         V_ = toArray(V)
@@ -130,15 +155,10 @@ def createPCExpansion(parameter_space = parameters, cvode_active=True):
 
         if cvode_active:
             inter = scipy.interpolate.InterpolatedUnivariateSpline(t_, V_, k=3)
-            #inter = scipy.interpolate.interp1d(t_, V_, kind="slinear")
-            #inter = scipy.interpolate.BarycentricInterpolator(t_, V_)
             solves.append((t_, V_, inter))
         else:
             solves.append((t_, V_))
 
-            
-        
-            
         i += 1
         
     sys.stdout.write("\rRunning Neuron: %2.1f%%" % (i/len(nodes.T)*100))
@@ -147,7 +167,6 @@ def createPCExpansion(parameter_space = parameters, cvode_active=True):
     
     solves = np.array(solves)
     if cvode_active:
-        # Fix so we have the same amount of timesteps for each simulation.
         lengths = []
         for s in solves[:,0]:
             lengths.append(len(s))
@@ -167,109 +186,68 @@ def createPCExpansion(parameter_space = parameters, cvode_active=True):
     return U_hat, dist, solves, t_max, P, nodes
 
 
-
-    
-#run(parameters)
-
 #subprocess.call(["rm figures/tmp_image_*"], shell=True)
 
-parameter_space =  newParameterSpaceUniform(fitted_parameters)
+#parameter_space =  newParameterSpaceNormal(fitted_parameters)
+
+def Normal(parameter):
+    return cp.Normal(parameter, abs(std_percentage*parameter))
+
+def Uniform(parameter):
+    return cp.Uniform(parameter - abs(uniform_interval*parameter),
+                      parameter + abs(uniform_interval*parameter))
+
+"""
+parameter_space =  newParameterSpace(fitted_parameters, Uniform)
 U_hat, dist, solves, t_max, P, nodes  = createPCExpansion(parameter_space, cvode_active=True)
-U_hat_const, dist_const, solves_const, t_max_const, P_const, nodes_const  = createPCExpansion(parameter_space, cvode_active=False)
+#U_hat_const, dist_const, solves_const, t_max_const, P_const, nodes_const  = createPCExpansion(parameter_space, cvode_active=False)
 
 
-interpolated_solves = []
-for inter in solves[:,2]:
-    interpolated_solves.append(inter(t_max_const))
-
-U_hat_const_like = cp.fit_regression(P, nodes, interpolated_solves, rule="LS")
-
-plt.clf()
+plt.figure()
 plt.plot(t_max, cp.E(U_hat, dist))
-plt.title("Interpolation mean")
+plt.title("Mean")
 plt.xlabel("Time")
 plt.ylabel("Voltage")
-plt.savefig(figurepath + "interpolation_test2_interpolation_mean" + figureformat)
-
-plt.clf()
-plt.plot(t_max_const, cp.E(U_hat_const, dist_const))
-plt.title("Constant dt mean")
-plt.xlabel("Time")
-plt.ylabel("Voltage")
-plt.savefig(figurepath + "interpolation_test2_constant_dt_mean" + figureformat)
+#plt.savefig(figurepath + "mean" + figureformat)
 
 
-plt.clf()
+plt.figure()
 plt.plot(t_max, cp.Var(U_hat, dist))
-plt.title("Interpolation variance")
+plt.title("Variance")
 plt.xlabel("Time")
 plt.ylabel("Voltage")
-plt.savefig(figurepath + "interpolation_test_interpolation_variance" + figureformat)
+#plt.savefig(figurepath + "variance" + figureformat)
+
+plt.show()
+"""
+
+def singleParameters(parameters = parameters):
+    for parameter in parameters:
+        print parameter
+    parameter_space =  newParameterSpace(fitted_parameters, Uniform)
+    U_hat, dist, solves, t_max, P, nodes  = createPCExpansion(parameter_space, cvode_active=True)
 
 
-plt.clf()
-plt.plot(t_max_const, cp.Var(U_hat_const, dist_const))
-plt.title("Constant dt variance")
-plt.xlabel("Time")
-plt.ylabel("Voltage")
-plt.savefig(figurepath + "interpolation_test2_constant_dt_variance" + figureformat)
+    plt.figure()
+    plt.plot(t_max, cp.E(U_hat, dist))
+    plt.title("Mean")
+    plt.xlabel("Time")
+    plt.ylabel("Voltage")
+    #plt.savefig(figurepath + "mean" + figureformat)
 
 
-plt.clf()
-plt.plot(t_max, cp.E(U_hat, dist))
-plt.plot(t_max_const, cp.E(U_hat_const, dist_const))
-plt.title("Mean constant dt vs interpolation")
-plt.legend(["Interpolation", "Constant dt"])
-plt.xlabel("Time")
-plt.ylabel("Voltage")
-plt.savefig(figurepath + "interpolation_test2_comparison_mean" + figureformat)
+    plt.figure()
+    plt.plot(t_max, cp.Var(U_hat, dist))
+    plt.title("Variance")
+    plt.xlabel("Time")
+    plt.ylabel("Voltage")
+    #plt.savefig(figurepath + "variance" + figureformat)
 
-
-plt.clf()
-plt.plot(t_max, cp.Var(U_hat, dist))
-plt.plot(t_max_const, cp.Var(U_hat_const, dist_const))
-plt.title("Variance constant dt vs interpolation")
-plt.legend(["Interpolation", "Constant dt"])
-plt.xlabel("Time")
-plt.ylabel("Voltage")
-plt.savefig(figurepath + "interpolation_test2_comparison_variance" + figureformat)
-
-plt.clf()
-plt.plot(t_max_const, abs(cp.E(U_hat_const, dist_const) - cp.E(U_hat_const_like, dist)))
-plt.title("Difference in mean, constant dt vs interpolation")
-plt.xlabel("Time")
-plt.ylabel("Voltage")
-plt.savefig(figurepath + "interpolation_test2_difference_mean" + figureformat)
-
-plt.clf()
-plt.plot(t_max_const, abs(cp.Var(U_hat_const, dist_const) - cp.Var(U_hat_const_like, dist)))
-plt.title("Difference in variance, constant dt vs interpolation")
-plt.xlabel("Time")
-plt.ylabel("Voltage")
-plt.savefig(figurepath + "interpolation_test2_difference_variance" + figureformat)
-
-plt.clf()
-plt.plot(t_max, cp.E(U_hat, dist))
-plt.plot(t_max_const, cp.E(U_hat_const_like, dist_const))
-plt.title("Mean constant dt vs interpolation")
-plt.legend(["Interpolation", "Constant dt"])
-plt.xlabel("Time")
-plt.ylabel("Voltage")
-plt.savefig(figurepath + "interpolation_test2_comparison_mean_same_points" + figureformat)
-
-plt.clf()
-plt.plot(t_max, cp.Var(U_hat, dist))
-plt.plot(t_max_const, cp.Var(U_hat_const_like, dist_const))
-plt.title("Variance constant dt vs interpolation")
-plt.legend(["Interpolation", "Constant dt"])
-plt.xlabel("Time")
-plt.ylabel("Voltage")
-plt.savefig(figurepath + "interpolation_test2_comparison_variance_same_points" + figureformat)
-
+    
 
 t_end = time.time()
 
-subprocess.Popen(["play","-q","ship_bell.wav"])
+#subprocess.Popen(["play","-q","ship_bell.wav"])
 
 #print "Interpolation time is: %g" % (t_constant_dt_end - t_interpolation_end)
 #print "Interpolation time is: %g" % (t_interpolation_end - t_interpolation_start)
