0b86159af5d4faf69aa3be00f974912fd8d666a2
Mikkel Elle Lepperød <lep.mik@gmail.com>
2016-03-01 09:49:03 +0100

    first version of nest-network

---
+++ nest_uncertainty_estimation.py
@@ -0,0 +1,37 @@
+import subprocess
+import datetime
+import uncertainpy
+
+memory = uncertainpy.utils.Memory(10)
+memory.start()
+
+parameterlist = [["J_E", 4, None],
+                 ["g", 4, None]]
+
+parameters = uncertainpy.Parameters(parameterlist)
+model = uncertainpy.models.NestNetwork(parameters)
+model.setAllDistributions(uncertainpy.Distribution(0.1).uniform)
+
+
+exploration = uncertainpy.UncertaintyEstimations(model,
+                                                 CPUs=8,
+                                                 save_figures=True,
+                                                 feature_list="all",
+                                                 output_dir_data="data/nest",
+                                                 output_dir_figures="figures/nest",
+                                                 nr_mc_samples=10**2)
+
+#percentages = [0.01, 0.03, 0.05, 0.07, 0.09, 0.11, 0.13, 0.15, 0.17, 0.19]
+# percentages = np.linspace(0.01, 0.25, 50)
+percentages = [0.01, 0.03, 0.05, 0.07, 0.09]
+
+test_distributions = {"uniform": percentages}
+exploration.exploreParameters(test_distributions)
+
+# mc_samples = [50, 100, 200, 500, 1000, 1500, 2000]
+# exploration.compareMC(mc_samples)
+
+memory.end()
+
+subprocess.Popen(["play", "-q", "ship_bell.wav"])
+print "The total runtime is: " + str(datetime.timedelta(seconds=(exploration.timePassed())))
---
+++ uncertainpy/models/NestNetwork.py
@@ -0,0 +1,91 @@
+from model import Model
+
+import numpy as np
+
+
+class NestNetwork(Model):
+    def __init__(self, parameters=None):
+        Model.__init__(self, parameters=parameters)
+        # Network parameters. These are given in Brunel (2000) J.Comp.Neuro.
+        self.g = 5.0  # Ratio of IPSP to EPSP amplitude: J_I/J_E
+        self.eta = 1.0  # rate of external population in multiples of threshold rate
+        self.delay = 1.5  # synaptic delay in ms
+        self.C_m = 281.0
+        self.tau_m = C_m/20.0   # Membrane time constant in mV
+        self.V_th = -50.4  # Spike threshold in mV
+        self.t_ref = 2.0
+        self.E_L = -60.
+        self.V_reset = -60.
+
+        self.J_E = 0.5  # J_I = -g*J_E
+
+        self.N_neurons = 10000
+        self.P_E = .8  # P_I = 1 - P_E
+
+        self.N_rec = 100   # Number of neurons to record from
+        self.simtime = 10000
+
+        nest.ResetKernel()
+
+        N_E = int(N_neurons * self.P_E)
+        N_I = int(N_neurons * (1 - self.P_E))
+
+        C_E = int(N_E/10)  # number of excitatory synapses per neuron
+        C_I = int(N_I/10)  # number of inhibitory synapses per neuron
+
+        J_I = -self.g * self.J_E
+        # rate of an external neuron in ms^-1
+        nu_ex = self.eta * abs(self.V_th) / (self.J_E * C_E * self.tau_m)
+        # rate of the external population in s^-1
+        p_rate = 1000.0 * nu_ex * C_E
+
+        # Configure kernel and neuron defaults
+        nest.SetKernelStatus({"print_time": True,
+                              "local_num_threads": 8})
+
+        nest.SetDefaults("iaf_psc_delta",
+                         {"C_m": self.C_m,
+                          "tau_m": self.tau_m,
+                          "t_ref": self.t_ref,
+                          "E_L": self.E_L,
+                          "V_th": self.V_th,
+                          "V_reset": self.V_reset})
+        # Create neurons
+        nodes = nest.Create("iaf_psc_delta", self.N_neurons)
+        nodes_E = nodes[:N_E]
+        nodes_I = nodes[N_E:]
+
+        # Connect neurons with each other
+        nest.CopyModel("static_synapse", "excitatory",
+                       {"weight": self.J_E, "delay": self.delay})
+
+        nest.Connect(nodes_E, nodes,
+                     {"rule": 'fixed_indegree', "indegree": C_E},
+                     "excitatory")
+
+        nest.CopyModel("static_synapse", "inhibitory",
+                       {"weight": J_I, "delay": delay})
+
+        nest.Connect(nodes_I, nodes,
+                     {"rule": 'fixed_indegree', "indegree": C_I},
+                     "inhibitory")
+
+        # Add stimulation and recording devices
+        noise = nest.Create("poisson_generator", params={"rate": p_rate})
+
+        # connect using all_to_all: one noise generator to all neurons
+        nest.Connect(noise, nodes, syn_spec="excitatory")
+
+        spikes = nest.Create("spike_detector", 2,
+                             params=[{"label": "Exc", "to_file": False},
+                                     {"label": "Inh", "to_file": False}])
+        spikes_E = spikes[:1]
+        spikes_I = spikes[1:]
+
+        # connect using all_to_all: all recorded excitatory neurons to one
+        # detector
+        nest.Connect(nodes_E[:self.N_rec], spikes_E)
+        nest.Connect(nodes_I[:self.N_rec], spikes_I)
+
+    def run(self):
+        nest.Simulate(self.simtime)
