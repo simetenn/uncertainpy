b01e5bb07bf252ec9c3f5909e4121de44ffa71c8
Simen Tennøe <simetenn@gmail.com>
2016-07-26 16:07:58 +0200

    removed nose2 as requirement and added tests.sh to jenkins

--- uncertainpy/install_scripts/install_all_dependencies.sh
+++ uncertainpy/install_scripts/install_all_dependencies.sh
@@ -57,6 +57,3 @@ pip_install -e git+https://github.com/hplgit/chaospy.git#egg=chaospy
 pip_install xvfbwrapper
 pip_install psutil
 pip_install -e git+https://github.com/hplgit/odespy.git#egg=odespy
-
-# Testing tools
-pip_install nose2
