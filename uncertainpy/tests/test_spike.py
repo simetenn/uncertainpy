df9bbd56fa02dfc72bbf830ce2b4a64da276d0e4
Simen Tennøe <simetenn@gmail.com>
2016-10-03 11:38:29 +0200

    split up spike files

---
+++ uncertainpy/tests/test_spike.py
@@ -0,0 +1,55 @@
+import numpy as np
+import os
+import unittest
+import shutil
+import subprocess
+
+from uncertainpy.features.spikes import Spike
+
+class TestSpike(unittest.TestCase):
+    def setUp(self):
+        t = np.arange(0, 10)
+        U = np.arange(0, 10) + 10
+        t_spike = 5
+        U_spike = 10
+        global_index = 50
+
+        self.spike = Spike(t, U, t_spike, U_spike, global_index,
+                           xlabel="time", ylabel="voltage")
+
+        self.output_test_dir = ".tests/"
+
+        if os.path.isdir(self.output_test_dir):
+            shutil.rmtree(self.output_test_dir)
+        os.makedirs(self.output_test_dir)
+
+
+    def tearDown(self):
+        if os.path.isdir(self.output_test_dir):
+            shutil.rmtree(self.output_test_dir)
+
+
+
+    def compare_plot(self, name):
+        folder = os.path.dirname(os.path.realpath(__file__))
+        compare_file = os.path.join(folder, "data/",
+                                    name + ".png")
+
+        plot_file = os.path.join(self.output_test_dir, name + ".png")
+
+        result = subprocess.call(["diff", plot_file, compare_file])
+        self.assertEqual(result, 0)
+
+    def test_init(self):
+        self.assertIsInstance(self.spike, Spike)
+        self.assertTrue(np.array_equal(self.spike.t, np.arange(0, 10)))
+        self.assertTrue(np.array_equal(self.spike.U, np.arange(0, 10) + 10))
+        self.assertEqual(self.spike.t_spike, 5)
+        self.assertEqual(self.spike.U_spike, 10)
+        self.assertEqual(self.spike.global_index, 50)
+
+
+    def test_plot(self):
+        self.spike.plot(os.path.join(self.output_test_dir, "spike.png"))
+
+        self.compare_plot("spike")
---
+++ uncertainpy/tests/test_spikes.py
@@ -0,0 +1,108 @@
+import numpy as np
+import os
+import unittest
+import shutil
+import subprocess
+
+from uncertainpy.features.spikes import Spike, Spikes
+
+
+class TestSpikes(unittest.TestCase):
+    def setUp(self):
+        folder = os.path.dirname(os.path.realpath(__file__))
+
+        self.t = np.load(os.path.join(folder, "data/t_test.npy"))
+        self.U = np.load(os.path.join(folder, "data/U_test.npy"))
+
+        self.output_test_dir = ".tests/"
+
+        if os.path.isdir(self.output_test_dir):
+            shutil.rmtree(self.output_test_dir)
+        os.makedirs(self.output_test_dir)
+
+
+    def tearDown(self):
+        if os.path.isdir(self.output_test_dir):
+            shutil.rmtree(self.output_test_dir)
+
+
+    def compare_plot(self, name):
+        folder = os.path.dirname(os.path.realpath(__file__))
+        compare_file = os.path.join(folder, "data/",
+                                    name + ".png")
+
+        plot_file = os.path.join(self.output_test_dir, name + ".png")
+
+        result = subprocess.call(["diff", plot_file, compare_file])
+        self.assertEqual(result, 0)
+
+
+    def test_init_no_input(self):
+        self.spikes = Spikes()
+        self.assertIsInstance(self.spikes, Spikes)
+
+
+    def test_init_input(self):
+        self.spikes = Spikes(self.t, self.U)
+        self.assertEqual(self.spikes.nr_spikes, 12)
+
+
+    def test_detectSpikesDefault(self):
+        self.spikes = Spikes()
+
+        self.spikes.detectSpikes(self.t, self.U)
+        self.assertEqual(self.spikes.nr_spikes, 12)
+
+
+    def test_detectSpikesAuto(self):
+        self.spikes = Spikes()
+
+        self.spikes.detectSpikes(self.t, self.U, thresh="auto")
+        self.assertEqual(self.spikes.nr_spikes, 12)
+
+
+    def test_detectSpikesExtended(self):
+        self.spikes = Spikes()
+
+        self.spikes.detectSpikes(self.t, self.U, extended_spikes=True)
+        self.assertEqual(self.spikes.nr_spikes, 12)
+
+
+    def test_detectSpikesDefaultAutoExtended(self):
+        self.spikes = Spikes()
+
+        self.spikes.detectSpikes(self.t, self.U, thresh="auto", extended_spikes=True)
+        self.assertEqual(self.spikes.nr_spikes, 12)
+
+
+    def test_iter(self):
+        self.spikes = Spikes()
+        self.spikes.detectSpikes(self.t, self.U)
+
+        for spike in self.spikes:
+            self.assertIsInstance(spike, Spike)
+
+    def test_len(self):
+        self.spikes = Spikes()
+        self.spikes.detectSpikes(self.t, self.U)
+
+        self.assertEqual(len(self.spikes), 12)
+
+
+    def test_getitem(self):
+        self.spikes = Spikes()
+        self.spikes.detectSpikes(self.t, self.U)
+
+        result = self.spikes[0]
+        self.assertIsInstance(result, Spike)
+
+
+    def test_plot(self):
+        self.spikes = Spikes(self.t, self.U, xlabel="xlabel", ylabel="ylabel")
+
+        self.spikes.plot(os.path.join(self.output_test_dir, "spikes.png"))
+        self.compare_plot("spikes")
+
+
+if __name__ == "__main__":
+    unittest.main()
