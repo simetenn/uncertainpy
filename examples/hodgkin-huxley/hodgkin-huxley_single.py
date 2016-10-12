46390b7abdddc9f1d11f16104e63ad1077a9f401
Simen Tennøe <simetenn@gmail.com>
2016-10-12 17:30:18 +0200

    updated hodgkin huxley, running ./install_dependencies.sh directly in travis

--- examples/hodgkin-huxley/hodgkin-huxley_single.py
+++ examples/hodgkin-huxley/hodgkin-huxley_single.py
@@ -1,15 +1,6 @@
 import uncertainpy
 import chaospy as cp
-import matplotlib.pyplot as plt
 
-# parameterlist = [["V_rest", 0, None],
-#                  ["Cm", 1, None],
-#                  ["gbar_Na", 120, None],
-#                  ["gbar_K", 36, None],
-#                  ["gbar_l", 0.3, None],
-#                  ["E_Na", 115, None],
-#                  ["E_K", -12, None],
-#                  ["E_l", 10.613, None]]
 
 def cdf(x, a, b, c, d):
     if x <= b:
@@ -19,8 +10,10 @@ def cdf(x, a, b, c, d):
     if x >= c:
         return (x-c+b)/(b-a + d-c)
 
+
 def bnd(x, a, b, c, d):
     return(a, d)
+
 
 def ppf(self, q, a, b, c, d):
     return (q < .5)*(2*q*(b-a) + a) + (q >= .5)*((2*q-1)*(d-c) + c)
@@ -29,9 +22,10 @@ def ppf(self, q, a, b, c, d):
 Distribution = cp.construct(cdf=cdf, bnd=bnd, ppf=ppf)
 distribution = Distribution(a=65, b=90, c=120, d=260)
 
+
 orignal_parameters = [["V_rest", 0, None],
                       ["Cm", 1, cp.Uniform(0.8, 1.5)],
-                      ["gbar_Na", 120, distribution],
+                      ["gbar_Na", 120, cp.Uniform(64, 260)],
                       ["gbar_K", 36, cp.Uniform(26, 49)],
                       ["gbar_l", 0.3, cp.Uniform(0.13, 0.5)],
                       ["E_Na", 115, cp.Uniform(95, 119)],
@@ -41,16 +35,11 @@ orignal_parameters = [["V_rest", 0, None],
 parameters = uncertainpy.Parameters(orignal_parameters)
 
 model = uncertainpy.HodkinHuxleyModel(parameters)
-# model.run()
-
-# uncertainpy.plotting.prettyPlot(model.t, model.U)
-# plt.show()
+features = uncertainpy.NeuronFeatures(features_to_run="all")
 
 exploration = uncertainpy.UncertaintyEstimation(model,
-                                                feature_list="all",
+                                                features=features,
                                                 CPUs=7,
-                                                output_dir_data="../../uncertainpy_results/data/hodgkin-huxley_single",
-                                                output_dir_figures="../../uncertainpy_results/figures/hodgkin-huxley_single",
                                                 save_figures=True)
 
 exploration.allParameters()
--- examples/hodgkin-huxley/hodgkin-huxley_uncertainty_estimation.py
+++ examples/hodgkin-huxley/hodgkin-huxley_uncertainty_estimation.py
@@ -1,34 +1,32 @@
 import subprocess
 import datetime
 import uncertainpy
-
-# memory = uncertainpy.Memory(10)
-# memory.start()
-
-# parameterlist = [["V_rest", 0, None],
-#                  ["gbar_Na", 120, None],
-#                  ["Cm", 1, None],
-#                  ["gbar_K", 36, None],
-#                  ["gbar_l", 0.3, None],
-#                  ["E_Na", 115, None],
-#                  ["E_K", -12], None,
-#                  ["E_l", 10.613, None]]
-
-parameterlist = [["gbar_Na", 120, None],
-                 ["gbar_K", 36, None],
-                 ["gbar_l", 0.3, None]]
+import chaospy as cp
 
 
-parameters = uncertainpy.Parameters(parameterlist)
+orignal_parameters = [["V_rest", 0, None],
+                      ["Cm", 1, cp.Uniform(0.8, 1.5)],
+                      ["gbar_Na", 120, cp.Uniform(65, 260)],
+                      ["gbar_K", 36, cp.Uniform(26, 49)],
+                      ["gbar_l", 0.3, cp.Uniform(0.13, 0.5)],
+                      ["E_Na", 115, cp.Uniform(95, 119)],
+                      ["E_K", -12, cp.Uniform(-9, -14)],
+                      ["E_l", 10.613, cp.Uniform(4, 22)]]
+
+
+
+
+parameters = uncertainpy.Parameters(orignal_parameters)
 
 model = uncertainpy.HodkinHuxleyModel(parameters)
 model.setAllDistributions(uncertainpy.Distribution(0.1).uniform)
 
+features = uncertainpy.NeuronFeatures(features_to_run="all")
+
+
 exploration = uncertainpy.UncertaintyEstimations(model,
-                                                 feature_list="all",
                                                  CPUs=7,
-                                                 output_dir_data="../../uncertainpy_results/data/hodgkin-huxley",
-                                                 output_dir_figures="../../uncertainpy_results/figures/hodgkin-huxley",
+                                                 features=features,
                                                  save_figures=True)
 
 percentages = [0.01, 0.03]
@@ -38,7 +36,5 @@ exploration.exploreParameters(test_distributions)
 mc_samples = [10, 100, 1000]
 exploration.compareMC(mc_samples)
 
-# memory.end()
 
-subprocess.call(["play", "-q", "ship_bell.wav"])
 print "The total runtime is: " + str(datetime.timedelta(seconds=(exploration.timePassed())))
