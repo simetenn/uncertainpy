38c22d99393d864e4a5df50ce02ff7bab8246704
Simen Tennøe <simetenn@gmail.com>
2016-10-12 17:43:24 +0200

    fixed bug in model __init__

--- uncertainpy/models/__init__.py
+++ uncertainpy/models/__init__.py
@@ -11,7 +11,6 @@ from IzhikevichModel import IzhikevichModel
 from NeuronModel import NeuronModel
 from NestNetwork import NestNetwork
 from model import Model
-from NeuronModelBahl import NeuronModelBahl
 from TestingModel import TestingModel0d, TestingModel1d, TestingModel2d
 from TestingModel import TestingModel0dNoTime, TestingModel1dNoTime, TestingModel2dNoTime
 from TestingModel import TestingModelNoU, TestingModel1dConstant, TestingModel1dAdaptive
