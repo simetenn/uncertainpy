6b313d9c6eb88fc535629b70e46c1dc77d7a2d6f
Simen Tennøe <simetenn@gmail.com>
2016-10-18 09:41:39 +0200

    added original hh model, a seperate file to reproduce the file

--- examples/hodgkin-huxley/hodgkin-huxley_uncertainty_estimation.py
+++ examples/hodgkin-huxley/hodgkin-huxley_uncertainty_estimation.py
@@ -23,54 +23,36 @@ Distribution = cp.construct(cdf=cdf, bnd=bnd, ppf=ppf)
 distribution = Distribution(a=65, b=90, c=120, d=260)
 
 
-orignal_parameters = [["V_rest", -75, None],
-                      ["Cm", 1, cp.Uniform(0.8, 1.5)],
-                      ["gbar_Na", 120, cp.Uniform(64, 260)],
-                      ["gbar_K", 36, cp.Uniform(26, 49)],
-                      ["gbar_l", 0.3, cp.Uniform(0.13, 0.5)],
-                      ["E_Na", 50, cp.Uniform(30, 54)],
-                      ["E_K", -77, cp.Uniform(-74, -79)],
-                      ["E_l", -50.613, cp.Uniform(-61, -43)],
-                      ["m0", 0.05, None],
-                      ["n0", 0.32, None],
-                      ["h0", 0.6, None]]
+parameters = [["V_rest", -75, None],
+              ["Cm", 1, cp.Uniform(0.8, 1.5)],
+              ["gbar_Na", 120, cp.Uniform(64, 260)],
+              ["gbar_K", 36, cp.Uniform(26, 49)],
+              ["gbar_l", 0.3, cp.Uniform(0.13, 0.5)],
+              ["E_Na", 50, cp.Uniform(30, 54)],
+              ["E_K", -77, cp.Uniform(-74, -79)],
+              ["E_l", -50.613, cp.Uniform(-61, -43)],
+              ["m0", 0.05, None],
+              ["n0", 0.32, None],
+              ["h0", 0.6, None]]
 
-orignal_parameters = [["Cm", 1, cp.Uniform(0.8, 1.5)],
-                    ["gbar_Na", 120, cp.Uniform(64, 260)],
-                    ["gbar_K", 36, cp.Uniform(26, 49)],
-                    ["gbar_l", 0.3, cp.Uniform(0.13, 0.5)],
-                    ["E_Na", 50, cp.Uniform(30, 54)],
-                    ["E_K", -77, cp.Uniform(-74, -79)],
-                    ["E_l", -50.613, cp.Uniform(-61, -43)]]
-
-# orignal_parameters = [["E_Na", 50, cp.Uniform(30, 54)],
-#                       ["E_K", -77, cp.Uniform(-74, -79)],
-#                       ["E_l", -50.613, cp.Uniform(-61, -43)]]
-#
-
-old_parameters = [["V_rest", -10, None],
-                  ["Cm", 1, cp.Uniform(0.8, 1.5)],
-                  ["gbar_Na", 120, cp.Uniform(64, 260)],
-                  ["gbar_K", 36, cp.Uniform(26, 49)],
-                  ["gbar_l", 0.3, cp.Uniform(0.13, 0.5)],
-                  ["m0", 0.0011, None],
-                  ["n0", 0.0003, None],
-                  ["h0", 0.9998, None],
-                  ["E_Na", 112, cp.Uniform(30, 54)],
-                  ["E_K", -12, cp.Uniform(-74, -79)],
-                  ["E_l", 10.613, cp.Uniform(-61, -43)]]
-
-# old_parameters = [["E_Na", 112, cp.Uniform(30, 54)],
-#                   ["E_K", -12, cp.Uniform(-74, -79)],
-#                   ["E_l", 10.613, cp.Uniform(-61, -43)]]
+# parameters = [["Cm", 1, cp.Uniform(0.8, 1.5)],
+#               ["gbar_Na", 120, cp.Uniform(64, 260)],
+#               ["gbar_K", 36, cp.Uniform(26, 49)],
+#               ["gbar_l", 0.3, cp.Uniform(0.13, 0.5)],
+#               ["E_Na", 50, cp.Uniform(30, 54)],
+#               ["E_K", -77, cp.Uniform(-74, -79)],
+#               ["E_l", -50.613, cp.Uniform(-61, -43)]]
 
 
-parameters = uncertainpy.Parameters(old_parameters)
+# parameters = [["E_Na", 50, cp.Uniform(30, 54)],
+#               ["E_K", -77, cp.Uniform(-74, -79)],
+#               ["E_l", -50.613, cp.Uniform(-61, -43)]]
 
-# model = uncertainpy.HodkinHuxleyModel(parameters=parameters)
 
-model = uncertainpy.HodkinHuxleyModelOld(parameters=parameters)
 
+parameters = uncertainpy.Parameters(parameters)
+
+model = uncertainpy.HodkinHuxleyModel(parameters=parameters)
 model.setAllDistributions(uncertainpy.Distribution(0.2).uniform)
 
 features = uncertainpy.NeuronFeatures(features_to_run="all")
---
+++ examples/hodgkin-huxley/hodgkin-huxley_valderrama.py
@@ -0,0 +1,36 @@
+import uncertainpy
+import chaospy as cp
+
+
+parameters = [["V_rest", -10, None],
+              ["Cm", 1, cp.Uniform(0.8, 1.5)],
+              ["gbar_Na", 120, cp.Uniform(64, 260)],
+              ["gbar_K", 36, cp.Uniform(26, 49)],
+              ["gbar_l", 0.3, cp.Uniform(0.13, 0.5)],
+              ["m0", 0.0011, None],
+              ["n0", 0.0003, None],
+              ["h0", 0.9998, None],
+              ["E_Na", 112, cp.Uniform(30, 54)],
+              ["E_K", -12, cp.Uniform(-74, -79)],
+              ["E_l", 10.613, cp.Uniform(-61, -43)]]
+
+# old_parameters = [["E_Na", 112, cp.Uniform(30, 54)],
+#                   ["E_K", -12, cp.Uniform(-74, -79)],
+#                   ["E_l", 10.613, cp.Uniform(-61, -43)]]
+
+
+parameters = uncertainpy.Parameters(parameters)
+
+model = uncertainpy.OriginalHodkinHuxleyModel(parameters=parameters)
+model.setAllDistributions(uncertainpy.Distribution(0.2).uniform)
+
+features = uncertainpy.NeuronFeatures(features_to_run="all")
+
+exploration = uncertainpy.UncertaintyEstimation(model,
+                                                seed=10,
+                                                features=features,
+                                                CPUs=7,
+                                                save_figures=True,
+                                                rosenblatt=True)
+
+exploration.allParameters()
