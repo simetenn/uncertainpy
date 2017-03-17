55c750991063d585bd191cc35023c9c3393ab9c8
Simen Tennøe <simetenn@gmail.com>
2017-03-17 16:19:51 +0100

    before changing features

--- tests/testing_classes/TestingFeatures.py
+++ tests/testing_classes/TestingFeatures.py
@@ -4,13 +4,13 @@ import numpy as np
 
 class TestingFeatures(GeneralFeatures):
     def feature0d(self):
-        return 1
+        return None, 1
 
     def feature1d(self):
-        return np.arange(0, 10)
+        return None, np.arange(0, 10)
 
     def feature2d(self):
-        return np.array([np.arange(0, 10), np.arange(0, 10)])
+        return None, np.array([np.arange(0, 10), np.arange(0, 10)])
 
     def featureInvalid(self):
-        return None
+        return None, None
