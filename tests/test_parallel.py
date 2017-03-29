36a7f8f39700a73cb229a1147f6e1ffb6713fea8
Simen Tennøe <simetenn@gmail.com>
2017-03-29 15:59:01 +0200

    fixed bug in test_parallel, added empty data file

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -207,7 +207,7 @@ class TestParallel(unittest.TestCase):
 
     def test_run_model_no_time(self):
         parallel = Parallel(model=TestingModelNoTime())
-        with self.assertRaises(ValueError):
+        with self.assertRaises(RuntimeError):
             parallel.run(self.model_parameters)
 
 
@@ -215,7 +215,7 @@ class TestParallel(unittest.TestCase):
         parallel = Parallel(model=TestingModel1d(),
                             features=TestingFeatures(features_to_run="feature_no_time"))
 
-        with self.assertRaises(ValueError):
+        with self.assertRaises(RuntimeError):
             parallel.run(self.model_parameters)
 
 
