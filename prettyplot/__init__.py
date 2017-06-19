470cf4d95259027f2ab041fadba8131443a76d36
Simen Tennøe <simetenn@gmail.com>
2017-06-19 16:00:02 +0200

    git subrepo pull prettyplot
    subrepo:
      subdir:   "prettyplot"
      merged:   "306cb49"
    upstream:
      origin:   "git@github.com:simetenn/prettyplot.git"
      branch:   "master"
      commit:   "306cb49"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

--- prettyplot/__init__.py
+++ prettyplot/__init__.py
@@ -1,10 +1,37 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.8"
+__version__ = "0.9"
 __author__ = "Simen Tennøe"
 
-# from prettyPlot import set_figuresize, set_legend, set_font, set_grid,
-# from prettyplot import spines_edge_color, remove_ticks, get_colormap_tableu20
-# from prettyplot import get_colormap, get_current_colormap, set_title, set_xlabel
-# from prettyplot import set_ylabel, set_style, prettyPlot, prettyBar
-from prettyplot import *
+from .prettyplot import set_figuresize
+from .prettyplot import set_legendstyle
+from .prettyplot import set_font
+from .prettyplot import set_linestyle
+from .prettyplot import set_tickstyle
+from .prettyplot import reset_style
+from .prettyplot import set_grid
+from .prettyplot import spines_color
+from .prettyplot import remove_ticks
+from .prettyplot import get_colormap_tableu20
+from .prettyplot import get_colormap
+from .prettyplot import get_current_colormap
+from .prettyplot import set_title
+from .prettyplot import set_xlabel
+from .prettyplot import set_axestyle
+from .prettyplot import set_ylabel
+from .prettyplot import set_style
+from .prettyplot import create_figure
+from .prettyplot import set_legend
+from .prettyplot import prettyPlot
+from .prettyplot import prettyBar
+
+from .prettyplot import axis_grey
+from .prettyplot import titlesize
+from .prettyplot import fontsize
+from .prettyplot import labelsize
+from .prettyplot import figsize
+from .prettyplot import ticksize
+from .prettyplot import ticklabelsize
+from .prettyplot import linewidth
+from .prettyplot import markersize
+from .prettyplot import markeredgewidth
