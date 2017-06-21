7eaa9abfd0ecb526f30904dc344a0ea1a7384ae9
Simen Tennøe <simetenn@gmail.com>
2017-06-21 14:30:14 +0200

    git subrepo clone https://github.com/simetenn/prettyplot src/uncertainpy/plotting/prettyplot/
    subrepo:
      subdir:   "src/uncertainpy/plotting/prettyplot"
      merged:   "9b20a5e"
    upstream:
      origin:   "https://github.com/simetenn/prettyplot"
      branch:   "master"
      commit:   "9b20a5e"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

---
+++ src/uncertainpy/plotting/prettyplot/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+
+except ImportError:
+    print("Setuptools is needed to install all dependencies")
+    print("Setuptools: https://pypi.python.org/pypi/setuptools")
+
+
+name = "prettyplot"
+version = "0.8"
+
+prettyplot_req = ["seaborn", "matplotlib", "numpy"]
+
+setup(name=name,
+      url="https://github.com/simetenn/prettyplot",
+      author="Simen Tennøe",
+      description='Pretty plotting',
+      platforms='linux',
+      packages=find_packages(),
+      install_requires=prettyplot_req
+      )
