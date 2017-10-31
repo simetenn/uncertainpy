cf034f7eb76a0eed45fd62ddfcc47d4b7f7e8507
Simen Tennøe <simetenn@gmail.com>
2017-10-31 15:58:20 +0100

    ignore files in coverage

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import platform
 if not platform.system() == "Linux":
     print("Warning: UncertainPy not tested for current operating system")
 
-name = "uncertainpy"
+name = "testme1"
 
 uncertainpy_req = ["xvfbwrapper", "chaospy", "tqdm", "h5py",
                    "multiprocess", "numpy", "scipy", "seaborn"]
@@ -26,15 +26,19 @@ anaconda_req = ["xvfbwrapper", "chaospy", "tqdm", "h5py",
                 "multiprocess", "numpy", "scipy", "seaborn",
                 "efel", "elephant",  "neo", "quantities"]
 
+long_description = open("README.md").read()
+
 packages = ['uncertainpy', 'uncertainpy.models', 'uncertainpy.features', 'uncertainpy.plotting', 'uncertainpy.utils']
-setup(name="uncertainpy",
-      version="0.9",
-      url="https://github.com/simetenn/uncertainpy",
-      author="Simen Tennøe",
-      description='Uncertainty quantification and sensitivity analysis',
-      platforms='linux',
+setup(name=name,
+      version="0.9.2",
+    #   url="https://github.com/simetenn/uncertainpy",
+    #   author="Simen Tennøe",
+    #   description='Uncertainty quantification and sensitivity analysis',
+    #   long_description=long_description,
+      python_requires=">=2.7",
       packages=find_packages("src"),
-      package_dir={"": "src", "uncertainpy.examples": "examples", "uncertainpy.tests": "tests",},
+      package_dir={"": "src"},
+      data_files=["README.md"],
       install_requires=anaconda_req,
       extras_require=extras_require,
       )
