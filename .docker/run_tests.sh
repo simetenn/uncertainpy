763a5af201223d19fb316f5a93ce11c77ef8d124
Simen Tennøe <simetenn@gmail.com>
2018-02-19 11:05:03 +0100

    TST: only relevant bug

--- .docker/run_tests.sh
+++ .docker/run_tests.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 source .bashrc
 cd uncertainpy
 python setup.py install --tests
-coverage run test.py all
+coverage run test.py uncertainty_calculations
 codecov --token=504e6f55-2e64-481b-88fe-8da7201c462e
