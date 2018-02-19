d383893ebbecefdf0ceed513017d7b06e45b4524
Simen Tennøe <simetenn@gmail.com>
2018-02-19 14:23:11 +0100

    TST: added all tests

--- .docker/run_tests.sh
+++ .docker/run_tests.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 source .bashrc
 cd uncertainpy
 python setup.py install --tests
-coverage run test.py uncertainty_calculations
+coverage run test.py all
 codecov --token=504e6f55-2e64-481b-88fe-8da7201c462e
