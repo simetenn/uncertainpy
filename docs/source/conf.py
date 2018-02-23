067df852135482c9d19b3cfea6864ab041a71849
Simen Tennøe <simetenn@gmail.com>
2018-02-23 13:13:08 +0100

    DOC: added source

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,9 @@ for mod_name in uncertainpy_require:
 # documentation rootime, valuesse os.path.abspath to make it absolute, like shown here.
 #
 # sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
-sys.path.insert(0, os.path.abspath("../src"))
+# sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../../src"))
+# sys.path.insert(0, os.path.abspath("../src/uncertainpy"))
 
 # -- General configuration ------------------------------------------------
 
