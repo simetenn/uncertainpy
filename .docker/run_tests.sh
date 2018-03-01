c9f60f15d554a8f1e1343cb0093307f7a47d72f0
Simen Tennøe <simetenn@gmail.com>
2018-03-01 15:12:59 +0100

    REL: bug

--- .docker/run_tests.sh
+++ .docker/run_tests.sh
@@ -5,5 +5,6 @@ source .bashrc
 cd uncertainpy
 python setup.py install --tests
 coverage run test.py spike
+echo DJDKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKkk?
 bash <(curl -s https://codecov.io/bash)
 # codecov --token=504e6f55-2e64-481b-88fe-8da7201c462e
