157815896864f80facbbb576b3ebf383bfb4ea5d
Simen Tennøe <simetenn@gmail.com>
2018-03-02 08:34:40 +0100

    REL: testing with dummy docker

--- .docker/run_tests.sh
+++ .docker/run_tests.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 source .bashrc
 cd uncertainpy
-python setup.py install --tests
-coverage run test.py spike
+#python setup.py install --tests
+#coverage run test.py spike
 #bash <(curl -s https://codecov.io/bash)
 ls -a
