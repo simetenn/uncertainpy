5b178464deaad81835a7a5fdecf0003ec4c7f9eb
Simen Tennøe <simetenn@gmail.com>
2018-04-13 14:42:22 +0200

    TST: updated compare files to new version of random generator

--- src/uncertainpy/utils/__init__.py
+++ src/uncertainpy/utils/__init__.py
@@ -3,7 +3,7 @@ Small utility functions for various purposes.
 """
 
 __all__ = ["create_logger", "lengths", "none_to_nan", "contains_nan",
-           "only_none_or_nan", "is_regular"]
+           "is_regular"]
 
 from .logger import create_logger
 from .utility import lengths, none_to_nan, contains_nan
