6d989abefed79cd94b46695a9272350066083550
Simen Tennøe <simetenn@gmail.com>
2018-04-24 10:47:12 +0200

    BUILD: complete install

--- run_tests.sh
+++ run_tests.sh
@@ -6,5 +6,5 @@ cd uncertainpy
 coverage run test.py all
 ls
 coverage xml
-# bash <(curl -s https://codecov.io/bash)
-mv coverage.xml ../shared/coverage.xml
+bash <(curl -s https://codecov.io/bash)
+# mv coverage.xml ../shared/coverage.xml
