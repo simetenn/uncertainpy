9f901c62722b899791fe03ef8d53e2bf8cd098f4
Simen Tennøe <simetenn@gmail.com>
2018-04-24 08:03:31 +0200

    BUILD: using run script

--- run_tests.sh
+++ run_tests.sh
@@ -2,10 +2,9 @@
 set -eo pipefail
 
 #source .bashrc
-# cd uncertainpy
-# python setup.py develop --tests
-pwd
+cd uncertainpy
 coverage run test.py all
 coverage xml
+ls
 # bash <(curl -s https://codecov.io/bash)
 mv coverage.xml ../shared/coverage.xml
