61be94df544bf3460e25621b38e91cbf1ac14c9b
Simen Tennøe <simetenn@gmail.com>
2018-04-24 14:12:50 +0200

    BUILD: cleaned up

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -eo pipefail
 
-#source .bashrc
 cd uncertainpy
 coverage run test.py all
 ls
 coverage xml
 bash <(curl -s https://codecov.io/bash)
-# mv coverage.xml ../shared/coverage.xml
