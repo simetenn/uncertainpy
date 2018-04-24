598dc3319908640d2398b12892b3ffb85af5f408
Simen Tennøe <simetenn@gmail.com>
2018-04-24 10:04:25 +0200

    BUILD mock build

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 #source .bashrc
 cd uncertainpy
 coverage run test.py all
-coverage xml
 ls
+coverage xml
 # bash <(curl -s https://codecov.io/bash)
 mv coverage.xml ../shared/coverage.xml
