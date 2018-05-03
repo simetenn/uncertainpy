7a98a53ca40fcee7374c4d4319ab88123df9124f
Simen Tennøe <simetenn@gmail.com>
2018-05-03 13:10:01 +0200

    Import new logger utility functions

--- src/uncertainpy/utils/__init__.py
+++ src/uncertainpy/utils/__init__.py
@@ -5,8 +5,8 @@ Small utility functions for various purposes.
 """
 
 __all__ = ["create_logger", "lengths", "none_to_nan", "contains_nan",
-           "is_regular"]
+           "is_regular", "MyFormatter", "TqdmLoggingHandler", "has_handlers"]
 
-from .logger import create_logger
+from .logger import create_logger, MyFormatter, TqdmLoggingHandler, has_handlers, load_config
 from .utility import lengths, none_to_nan, contains_nan
 from .utility import is_regular, set_nan
