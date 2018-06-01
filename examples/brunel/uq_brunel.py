307e24f8735ce2fddb75a81da3ea3b367cc5aeb9
Simen Tennøe <simetenn@gmail.com>
2018-06-01 18:50:15 +0200

    Updated example

--- examples/brunel/uq_brunel.py
+++ examples/brunel/uq_brunel.py
@@ -4,8 +4,11 @@ import chaospy as cp
 from brunel import brunel_network
 
 # Create a Nest model from the brunel network function
+# We set ignore=True since we are not interested in
+# the model result itself. This is recommended as long as you do not
+# need the model results, since performing PCE for the model results
+# take much time
 model = un.NestModel(run=brunel_network, ignore=True)
-
 
 # Parametes for the synchronous regular (SR) state
 parameters = {"eta": cp.Uniform(1.5, 3.5),
@@ -22,17 +25,17 @@ parameters = {"eta": cp.Uniform(1.5, 2.2),
 parameters_AI = un.Parameters(parameters)
 
 # Initialize network features
-features = un.NetworkFeatures(features_to_run=["mean_isi"])
+features = un.NetworkFeatures()
 
 # Set up the problem
 UQ = un.UncertaintyQuantification(model,
                                   parameters=parameters_SR,
                                   features=features)
 
-# # Perform uncertainty quantification
-# # and save the data and plots under their own name
-# UQ.quantify(figure_folder="figures_brunel_SR",
-#             filename="brunel_SR_test")
+# Perform uncertainty quantification
+# and save the data and plots under their own name
+UQ.quantify(figure_folder="figures_brunel_SR",
+            filename="brunel_SR")
 
 # Change the set of parameters
 UQ.parameters = parameters_AI
@@ -40,4 +43,4 @@ UQ.parameters = parameters_AI
 # Perform uncertainty quantification on the new parameter set
 # and save the data and plots under their own name
 data = UQ.quantify(figure_folder="figures_brunel_AI",
-                   filename="brunel_AI_test")
+                   filename="brunel_AI")
