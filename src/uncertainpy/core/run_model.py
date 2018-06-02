Bug: NeuronModel now can run for more than ~1200 iterations withouth segment overflow

src/uncertainpy/core/run_model.py
@@ -410,7 +410,9 @@ class RunModel(ParameterBase):
 
 
         # pool.map(self._parallel.run, model_parameters)
-        for result in tqdm(pool.imap(self._parallel.run, model_parameters),
+        # chunksize = int(np.ceil(len(model_parameters)/self.CPUs))
+        chunksize = 1
+        for result in tqdm(pool.imap(self._parallel.run, model_parameters, chunksize),
                            desc="Running model",
                            total=len(nodes.T)):
 

src/uncertainpy/models/neuron_model.py
@@ -121,6 +121,9 @@ class NeuronModel(Model):
         if name:
             self.name = name
 
+        self.time = None
+        self.V = None
+
         setup_module_logger(class_instance=self, level=logger_level)
 
 
@@ -138,7 +141,13 @@ class NeuronModel(Model):
             raise ImportError("NeuronModel requires: neuron")
 
         self.h = neuron.h
-        self.h.load_file(1, self.file.encode())
+
+        # self.h("forall delete_section()")
+        self.h.load_file(0, self.file.encode())
+        # self.h.xopen(self.file.encode())
+        # for section in self.h.allsec():
+        #     print(section)
+        # self.h.topology()
 
         os.chdir(current_dir)
 
@@ -183,22 +192,33 @@ class NeuronModel(Model):
         RuntimeError
             If no section with name ``soma`` is found in the Neuron model.
         """
-        self.V = None
-        for section in self.h.allsec():
-            if section.name().lower() == "soma":
-                self.V = self.h.Vector()
-                self.V.record(section(0.5)._ref_v)
-                break
+        # if not hasattr(self.h, "soma"):
+        #     raise RuntimeError("No section with name soma found in: {}. Unable to record from soma".format(self.name))
 
-        if self.V is None:
-            raise RuntimeError("Soma not found in Neuron model: {model_name}".format(self.name))
+        if not hasattr(self.h, "voltage_soma"):
+            # self.h("objref voltage_soma")
+            # self.h("voltage_soma = new Vector()")
+
+            # self.h.voltage_soma.record(self.h.soma(0.5)._ref_v)
+
+            for section in self.h.allsec():
+                if section.name().lower() == "soma":
+                    self.h("objref voltage_soma")
+                    self.h("voltage_soma = new Vector()")
+
+                    self.h.voltage_soma.record(section(0.5)._ref_v)
+                    break
+
+        if not hasattr(self.h, "voltage_soma"):
+            raise RuntimeError("No section with name soma found in: {}. Unable to record from soma".format(self.name))
 
 
     def _record_t(self):
         """
         Record time values
         """
-        self.time = self._record("_ref_t")
+        if self.time is None:
+            self.time = self._record("_ref_t")
 
 
 
@@ -239,8 +259,12 @@ class NeuronModel(Model):
 
         self.h.run()
 
-        values = self._to_array(self.V)
+        values = np.array(self.h.voltage_soma.to_python())
+        # values = self._to_array(self.V)
         time = self._to_array(self.time)
+
+        # self.V.resize(0)
+        # self.time.resize(0)
 
         return time, values, self.info
 