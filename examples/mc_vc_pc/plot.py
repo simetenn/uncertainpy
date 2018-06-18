fb4f3079fd0ceca731860c073607a623365ce14f
Simen Tennøe <simetenn@gmail.com>
2018-06-18 13:18:28 +0200

    Updated mc_vs_pc plot

--- examples/mc_vc_pc/plot.py
+++ examples/mc_vc_pc/plot.py
@@ -1,7 +1,11 @@
 import h5py
+import numpy as np
 import matplotlib.pyplot as plt
 
-from uncertainpy.plotting.prettyplot.prettyplot import prettyPlot, set_latex_font, set_style
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
+
+from uncertainpy.plotting.prettyplot.prettyplot import prettyPlot, prettyBar, set_latex_font, set_style, get_current_colormap
 
 # plotting options
 figure_width = 7.08
@@ -13,7 +17,6 @@ linewidth = 1
 figsize = (figure_width, figure_width*0.4)
 figure_format = ".eps"
 dpi = 300
-
 
 # Load the analysed data
 with h5py.File("pc_mc.h5", "r") as f:
@@ -47,47 +50,60 @@ with h5py.File("pc_mc.h5", "r") as f:
 set_latex_font()
 set_style("seaborn-darkgrid")
 
-fig, axes = plt.subplots(ncols=2, figsize=figsize)
+fig, axes = plt.subplots(ncols=2, figsize=figsize, sharey=True)
 
 nr_colors = 2
 
 # 3 uncertain parameters
-prettyPlot(pc_evaluations_3, pc_mean_errors_3, nr_colors=nr_colors, color=0, ax=axes[0], label="Polynomial chaos, mean")
-prettyPlot(pc_evaluations_3, pc_variance_errors_3, nr_colors=nr_colors, color=0, linestyle="--", ax=axes[0], label="Polynomial chaos, variance")
-prettyPlot(pc_sobol_evaluations_3, pc_sobol_errors_3, nr_colors=nr_colors, color=0, linestyle=":", ax=axes[0], label="Polynomial chaos, Sobol first")
+prettyPlot(pc_evaluations_3, pc_mean_errors_3, linewidth=linewidth, nr_colors=nr_colors, color=0, ax=axes[0])
+prettyPlot(pc_evaluations_3, pc_variance_errors_3, linewidth=linewidth, nr_colors=nr_colors, color=0, linestyle="--", ax=axes[0])
+prettyPlot(pc_sobol_evaluations_3, pc_sobol_errors_3, linewidth=linewidth, nr_colors=nr_colors, color=0, linestyle=":", ax=axes[0])
 
 
-prettyPlot(mc_evaluations_3, mc_mean_errors_3, nr_colors=nr_colors, color=1, ax=axes[0], label="quasi-Monte Carlo, mean")
-prettyPlot(mc_evaluations_3, mc_variance_errors_3, nr_colors=nr_colors, color=1, linestyle="--", ax=axes[0], label="quasi-Monte Carlo, variance")
-prettyPlot(mc_sobol_evaluations_3, mc_sobol_errors_3, nr_colors=nr_colors, color=1, linestyle=":", ax=axes[0], label="quasi-Monte Carlo, Sobol first")
+prettyPlot(mc_evaluations_3, mc_mean_errors_3,  linewidth=linewidth, nr_colors=nr_colors, color=1, ax=axes[0])
+prettyPlot(mc_evaluations_3, mc_variance_errors_3, linewidth=linewidth, nr_colors=nr_colors, color=1, linestyle="--", ax=axes[0])
+prettyPlot(mc_sobol_evaluations_3, mc_sobol_errors_3, linewidth=linewidth, nr_colors=nr_colors, color=1, linestyle=":", ax=axes[0])
 
-axes[0].set_title("A) 3 uncertain parameters", fontsize=titlesize)
-axes[0].set_ylabel("Average absolute relative\nerror over time", fontsize=labelsize)
-axes[0].set_xlabel("Number of model evaluations", fontsize=labelsize)
+axes[0].set_title("A) Three uncertain parameters", fontsize=titlesize)
+axes[0].set_ylabel("Average absolute relative\nerror over time, $\\varepsilon$", fontsize=labelsize)
+axes[0].set_xlabel("Number of model evaluations, $N_s$", fontsize=labelsize)
 axes[0].set_yscale("log")
 axes[0].set_xlim([0, 2000])
-axes[0].set_ylim([5*10**-7, 10**11])
-axes[0].legend(fontsize=fontsize)
+axes[0].legend(fontsize=fontsize, ncol=2)
 
+grey = (0.5, 0.5, 0.5)
+colors = get_current_colormap()
+legend_elements = [Patch(facecolor=colors[0], label="PC"),
+                   Patch(facecolor=colors[1], label="QMC"),
+                   Line2D([0], [0], linewidth=0, label=""),
+                   Line2D([0], [0], color=grey, linewidth=linewidth, label="Mean"),
+                   Line2D([0], [0], color=grey, linewidth=linewidth, linestyle="--", label="Variance"),
+                   Line2D([0], [0], color=grey, linewidth=linewidth, linestyle=":", label="First-order Sobol")]
+
+axes[0].legend(handles=legend_elements, fontsize=fontsize, ncol=2)
 
 
 # 11 uncertain parameters
-prettyPlot(pc_evaluations_11, pc_mean_errors_11, nr_colors=nr_colors, color=0, ax=axes[1], label="Polynomial chaos, mean")
-prettyPlot(pc_evaluations_11, pc_variance_errors_11, nr_colors=nr_colors, color=0, linestyle="--", ax=axes[1], label="Polynomial chaos, variance")
-prettyPlot(pc_sobol_evaluations_11, pc_sobol_errors_11, nr_colors=nr_colors, color=0, linestyle=":", ax=axes[1], label="Polynomial chaos, Sobol first")
+prettyPlot(pc_evaluations_11, pc_mean_errors_11, linewidth=linewidth, nr_colors=nr_colors, color=0, ax=axes[1])
+prettyPlot(pc_evaluations_11, pc_variance_errors_11, linewidth=linewidth, nr_colors=nr_colors, color=0, linestyle="--", ax=axes[1])
+prettyPlot(pc_sobol_evaluations_11, pc_sobol_errors_11, linewidth=linewidth, nr_colors=nr_colors, color=0, linestyle=":", ax=axes[1])
 
-prettyPlot(mc_evaluations_11, mc_mean_errors_11, nr_colors=nr_colors, color=1, ax=axes[1], label="quasi-Monte Carlo, mean")
-prettyPlot(mc_evaluations_11, mc_variance_errors_11, nr_colors=nr_colors, color=1, linestyle="--", ax=axes[1], label="quasi-Monte Carlo, variance")
-prettyPlot(mc_sobol_evaluations_11, mc_sobol_errors_11, nr_colors=nr_colors, color=1, linestyle=":", ax=axes[1], label="quasi-Monte Carlo, Sobol first")
+prettyPlot(mc_evaluations_11, mc_mean_errors_11, linewidth=linewidth, nr_colors=nr_colors, color=1, ax=axes[1])
+prettyPlot(mc_evaluations_11, mc_variance_errors_11, linewidth=linewidth, nr_colors=nr_colors, color=1, linestyle="--", ax=axes[1])
+prettyPlot(mc_sobol_evaluations_11, mc_sobol_errors_11, linewidth=linewidth, nr_colors=nr_colors, color=1, linestyle=":", ax=axes[1])
 
 
-axes[1].set_title("B) 11 uncertain parameters", fontsize=titlesize)
-axes[1].set_ylabel("Average absolute relative\nerror over time", fontsize=labelsize)
-axes[1].set_xlabel("Number of model evaluations", fontsize=labelsize)
+
+axes[1].set_title("B) Eleven uncertain parameters", fontsize=titlesize)
+axes[1].set_ylabel("Average absolute relative\nerror over time, $\\varepsilon$", fontsize=labelsize)
+axes[1].set_xlabel("Number of model evaluations, $N_s$", fontsize=labelsize)
 axes[1].set_yscale("log")
 axes[1].set_xlim([0, 10000])
-axes[1].set_ylim([10**-6, 10**14])
-axes[1].legend(fontsize=fontsize)
+axes[1].set_ylim([5*10**-7, 10**8])
+axes[1].set_yticks([10**-6, 10**-3, 10**0, 10**3, 10**6])
+axes[1].legend(handles=legend_elements, fontsize=fontsize, ncol=2)
+
+
 
 plt.tight_layout()
 plt.savefig("mc_vs_pc" + figure_format, dpi=dpi)
