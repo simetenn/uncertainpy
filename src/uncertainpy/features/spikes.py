Added normalize option for spike calculation, fixed bug in end_threshold

src/uncertainpy/features/spikes.py
@@ -205,14 +205,18 @@ class Spikes:
         considered a spike. If "auto" the threshold is set to the standard
         deviation of the voltage trace. Default is -30.
     end_threshold : {int, float}, optional
-        The end threshold for a spike relative to the threshold.
-        Default is -10.
+        The end threshold for a spike relative to the threshold. Generally
+        negative values give the best results. Default is -10.
     extended_spikes : bool
         If the spikes should be extended past the threshold, until the
         derivative of the voltage trace is below 0.5. Default is False.
     trim : bool, optional
         If the spikes should be trimmed back from the termination threshold,
         so each spike is equal the threshold at both ends. Default is True.
+    normalize : bool, optional
+        If the voltage traceshould be normalized before the spikes are
+        found. If normalize is used threshold must be between [0, 1], and
+        the end_threshold a similar relative value. Default is False.
     xlabel : str, optional
         Label for the x-axis.
     ylabel : str, optional
@@ -255,8 +259,10 @@ class Spikes:
                  end_threshold=-10,
                  extended_spikes=False,
                  trim=True,
+                 normalize=False,
                  xlabel="",
                  ylabel=""):
+
         self.spikes = []
         self.nr_spikes = 0
 
@@ -271,7 +277,8 @@ class Spikes:
                              threshold=threshold,
                              end_threshold=end_threshold,
                              extended_spikes=extended_spikes,
-                             trim=trim)
+                             trim=trim,
+                             normalize=normalize)
 
 
     def __iter__(self):
@@ -332,11 +339,14 @@ class Spikes:
         return self.spikes[i]
 
 
-    def find_spikes(self, time, V,
+    def find_spikes(self,
+                    time,
+                    V,
                     threshold=-30,
                     end_threshold=-10,
                     extended_spikes=False,
-                    trim=True):
+                    trim=True,
+                    normalize=False):
         """
         Finds spikes in the given voltage trace.
 
@@ -352,15 +362,27 @@ class Spikes:
             considered a spike. If "auto" the threshold is set to the standard
             deviation of the voltage trace. Default is -30.
         end_threshold : {int, float}, optional
-            The end threshold for a spike relative to the threshold.
-            Default is -10.
+            The end threshold for a spike relative to the threshold. Generally
+            negative values give the best results. Default is -10.
         extended_spikes : bool, optional
             If the spikes should be extended past the threshold, until the
             derivative of the voltage trace is below 0.5. Default is False.
         trim : bool, optional
             If the spikes should be trimmed back from the termination threshold,
             so each spike is equal the threshold at both ends. Default is True.
+        normalize : bool, optional
+            If the voltage traceshould be normalized before the spikes are
+            found. If normalize is used threshold must be between [0, 1], and
+            the end_threshold must have a absolute value between [0, 1]. Default
+            is False.
 
+        Raises
+        ------
+        ValueError
+            If the threshold is outside the interval [0, 1] when normalize=True.
+        ValueError
+            If the absolute value of end_threshold is outside the
+            interval [0, 1] when normalize=True.
 
         Notes
         -----
@@ -378,18 +400,35 @@ class Spikes:
         self.time = time
         self.V = V
 
+
+        # Normalize the values
+        if normalize:
+            if threshold > 1 or threshold < 0:
+                raise ValueError("Threshold must be between [0, 1] when normalize=True")
+
+            if abs(end_threshold) > 1 or abs(end_threshold) < 0:
+                raise ValueError("Absolute value of end_threshold must be between [0, 1] when normalize=True")
+
+            voltage = V.copy()
+            voltage -= voltage.min()
+            voltage /= voltage.max()
+        else:
+            voltage = V
+
+
+
         min_extent_from_peak = 1
         derivative_cutoff = 0.5
 
         self.spikes = []
         if threshold == "auto":
-            threshold = np.sqrt(V.var())
+            threshold = np.sqrt(voltage.var())
 
         spike_start = 0
         start_flag = False
 
         if extended_spikes:
-            dVdt = np.gradient(V)
+            dVdt = np.gradient(voltage)
 
             gt_derivative = np.where(dVdt >= derivative_cutoff)[0]
             lt_derivative = np.where(dVdt <= -derivative_cutoff)[0]
@@ -397,12 +436,12 @@ class Spikes:
         prev_spike_end = 0
 
         for i in range(len(V)):
-            if V[i] > threshold and start_flag is False:
+            if voltage[i] > threshold and start_flag is False:
                 spike_start = i
                 start_flag = True
                 continue
 
-            elif V[i] < (threshold + end_threshold) and start_flag is True:
+            elif voltage[i] < (threshold + end_threshold) and start_flag is True:
                 start_flag = False
                 spike_end = i + 1
 

src/uncertainpy/features/spiking_features.py
@@ -51,6 +51,9 @@ class SpikingFeatures(GeneralSpikingFeatures):
         The threshold where the model result is considered to have a spike.
         If "auto" the threshold is set to the standard variation of the
         result. Default is -30.
+    end_threshold : {int, float}, optional
+        The end threshold for a spike relative to the threshold. Generally
+        negative values give the best results. Default is -10.
     extended_spikes : bool, optional
         If the found spikes should be extended further out than the threshold
         cuttoff. If True the spikes is considered to start and end where the
@@ -83,6 +86,8 @@ class SpikingFeatures(GeneralSpikingFeatures):
         A Spikes object that contain all spikes.
     threshold : {float, int}
         The threshold where the model result is considered to have a spike.
+    end_threshold : {int, float}, optional
+        The end threshold for a spike relative to the threshold.
     extended_spikes : bool
         If the found spikes should be extended further out than the threshold
         cuttoff.
@@ -111,10 +116,10 @@ class SpikingFeatures(GeneralSpikingFeatures):
     nr_spikes                   time_before_first_spike
     spike_rate                  average_AP_overshoot
     average_AHP_depth           average_AP_width
-    accommodation_index
+    accommodation_index         average_duration
     ==========================  ==========================
 
-    The features are from:
+    Most of the feature are from:
     Druckmann, S., Banitt, Y., Gidon, A. A., Schurmann, F., Markram, H., and Segev, I.
     (2007). A novel multiple objective optimization framework for constraining conductance-
     based neuron models by experimental data. Frontiers in Neuroscience 1, 7-18. doi:10.
@@ -130,6 +135,7 @@ class SpikingFeatures(GeneralSpikingFeatures):
                  features_to_run="all",
                  interpolate=None,
                  threshold=-30,
+                 end_threshold=-10,
                  extended_spikes=False,
                  labels={},
                  strict=True,
@@ -144,13 +150,15 @@ class SpikingFeatures(GeneralSpikingFeatures):
                               "accommodation_index": ["Accommodation index"],
                               "average_AP_overshoot": ["Voltage (mV)"],
                               "average_AHP_depth": ["Voltage (mV)"],
-                              "average_AP_width": ["Time (ms)"]
+                              "average_AP_width": ["Time (ms)"],
+                              "average_duration": ["Time (ms)"]
                              }
 
         super(SpikingFeatures, self).__init__(new_features=new_features,
                                               features_to_run=features_to_run,
                                               interpolate=interpolate,
                                               threshold=threshold,
+                                              end_threshold=end_threshold,
                                               extended_spikes=extended_spikes,
                                               labels=implemented_labels,
                                               logger_level=logger_level)
@@ -464,10 +472,9 @@ class SpikingFeatures(GeneralSpikingFeatures):
 
     def average_duration(self, time, spikes, info):
         """
-        The average action potential width.
+        The average duration of an action potential, from the action potential
+        onset to action potential termination.
 
-        The average of the width of every spike (action potential) at the
-        midpoint between the start and maximum of each spike.
 
         Parameters
         ----------
@@ -485,28 +492,28 @@ class SpikingFeatures(GeneralSpikingFeatures):
             The average action potential width. Returns None if there are
             no spikes in the model result.
         """
-        # if spikes.nr_spikes <= 0:
-        #     return None, None
+        if spikes.nr_spikes <= 0:
+            return None, None
 
-        # sum_duration = 0
-        # for spike in spikes:
-        #     sum_duration += spike.time[-1] - spike.time[0]
-
-        # return None, sum_duration/float(spikes.nr_spikes)
-
-        voltage = spikes.V
-        normalized_voltage = voltage - voltage.min()
-        normalized_voltage /= normalized_voltage.max()
-
-        # Find spikes in the normalized voltage trace
-        spikes = Spikes(time, normalized_voltage, threshold=0.55, end_threshold=-0.1)
-
-        # Calculate the duration of each spike
-        duration = []
+        sum_duration = 0
         for spike in spikes:
-            duration.append(spike.time[-1] - spike.time[0])
+            sum_duration += spike.time[-1] - spike.time[0]
 
-        return None, np.sum(duration)
+        return None, sum_duration/float(spikes.nr_spikes)
+
+        # voltage = spikes.V
+        # normalized_voltage = voltage - voltage.min()
+        # normalized_voltage /= normalized_voltage.max()
+
+        # # Find spikes in the normalized voltage trace
+        # spikes = Spikes(time, normalized_voltage, threshold=0.55, end_threshold=-0.1)
+
+        # # Calculate the duration of each spike
+        # duration = []
+        # for spike in spikes:
+        #     duration.append(spike.time[-1] - spike.time[0])
+
+        # return None, np.sum(duration)
 
 
     def accommodation_index(self, time, spikes, info):

tests/test_features.py
@@ -270,12 +270,25 @@ class TestGeneralSpikingFeatures(unittest.TestCase):
         self.assertTrue(np.array_equal(self.time, time))
 
 
+    def test_preprocess_normalize(self):
+        self.features = GeneralSpikingFeatures(logger_level="error", normalize=True, threshold=0.4, end_threshold=-0.1)
+
+        time, spikes, info = self.features.preprocess(self.time, self.values, self.info)
+
+        self.assertEqual(spikes.nr_spikes, 12)
+
+        self.assertIsInstance(spikes, Spikes)
+        self.assertTrue(np.array_equal(self.time, time))
+        self.assertTrue(spikes[0].V_spike > 1)
+
+
     def test_reference_feature(self):
         self.features = GeneralSpikingFeatures(logger_level="error")
         time, values = self.features.reference_feature(1, 1, 1)
 
         self.assertIsNone(time)
         self.assertIsNone(values)
+
 
 
 class TestSpikingFeatures(unittest.TestCase):
@@ -288,7 +301,7 @@ class TestSpikingFeatures(unittest.TestCase):
         self.implemented_features = ["nr_spikes", "time_before_first_spike",
                                      "spike_rate", "average_AP_overshoot",
                                      "average_AHP_depth", "average_AP_width",
-                                     "accommodation_index"]
+                                     "accommodation_index", "average_duration"]
 
         self.implemented_labels = {"nr_spikes": ["Number of spikes"],
                                    "spike_rate": ["Spike rate (Hz)"],
@@ -296,7 +309,8 @@ class TestSpikingFeatures(unittest.TestCase):
                                    "accommodation_index": ["Accommodation index"],
                                    "average_AP_overshoot": ["Voltage (mV)"],
                                    "average_AHP_depth": ["Voltage (mV)"],
-                                   "average_AP_width": ["Time (ms)"]
+                                   "average_AP_width": ["Time (ms)"],
+                                   "average_duration": ["Time (ms)"]
                                    }
 
         self.features = SpikingFeatures(logger_level="error")
@@ -331,7 +345,8 @@ class TestSpikingFeatures(unittest.TestCase):
                   "accommodation_index": ["Accommodation index"],
                   "average_AP_overshoot": ["Voltage (mV)"],
                   "average_AHP_depth": ["Voltage (mV)"],
-                  "average_AP_width": ["Time (ms)"]
+                  "average_AP_width": ["Time (ms)"],
+                  "average_duration": ["Time (ms)"]
                   }
 
         self.assertEqual(features.labels, labels)
@@ -435,6 +450,11 @@ class TestSpikingFeatures(unittest.TestCase):
         self.assertLess(self.features.average_AP_width(self.time, self.spikes, self.info)[1], 5)
 
 
+    # TODO Find correct testime, this is a rough bound only
+    def test_average_duration(self):
+        self.assertLess(self.features.average_duration(self.time, self.spikes, self.info)[1], 5)
+
+
     def test_average_AP_widthNone(self):
         self.features.spikes.nr_spikes = 0
         self.assertEqual(self.features.average_AP_width(self.time, self.spikes, self.info), (None, None))
@@ -451,6 +471,18 @@ class TestSpikingFeatures(unittest.TestCase):
 
 
     def test_calculate_all_features(self):
+
+        result = self.features.calculate_all_features(self.t, self.V, self.info)
+        self.assertEqual(set(result.keys()),
+                         set(self.implemented_features))
+
+
+    def test_calculate_all_features_normalize(self):
+        self.features = SpikingFeatures(logger_level="error", threshold=0.4, end_threshold=-0.1)
+
+        self.info = {"stimulus_start": self.t[0], "stimulus_end": self.t[-1]}
+
+        self.time, self.spikes, info = self.features.preprocess(self.t, self.V, self.info)
 
         result = self.features.calculate_all_features(self.t, self.V, self.info)
         self.assertEqual(set(result.keys()),